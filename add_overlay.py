e520cf34991c4354f225f3fa4ec76482373dfde3
M. Reza <ironcladgeek@hotmail.com>
2021-02-06 01:07:55 +0330

    refactor add_face_mask function

--- add_overlay.py
+++ add_overlay.py
@@ -6,12 +6,14 @@ import shutil
 from utils import get_image_files
 
 
-def add_face_mask(src_path, mask_colors=['white', 'black', 'blue', 'red']):
-    """Add face make to images if detect a face in the image. Nothing happens if no face detected in the image.
-    Copy the original images plus mask-added ones into a new directory named <original-directory>_with_mask
+def add_face_mask(src_path, dst_path=None, copy_originals_to_dst=True, mask_colors=['white', 'black', 'blue', 'red']):
+    """
+    Add face make to images if detect a face in the image. Nothing happens if no face detected in the image.
 
-    :param src_path (str, PosixPath): Path to directory of images
-    :param mask_colors (list): List of mask colors to be added. Could be 'white', 'black', 'blue' and 'red'.
+    :param src_path (str, PosixPath): Path to directory of images.
+    :param dst_path (str, PosixPath): If not None, the mask added images will be saved here, otherwise, in src_path.
+    :param copy_originals_to_dst (boolean): If true, copy images found in src_path to dst_path.
+    :param mask_colors (list[str]): List of mask colors to be added. Could be 'white', 'black', 'blue' and 'red'.
     """
     # check the color masks
     for color in mask_colors:
@@ -19,9 +21,12 @@ def add_face_mask(src_path, mask_colors=['white', 'black', 'blue', 'red']):
             raise NameError('Mask colors should be white, black, blue or red.')
 
     src_path = Path(src_path)
-    path = src_path.absolute().parent / f'{src_path.name}_with_mask'
-    shutil.copytree(src_path, path)         # copy original images into dest directory
-    fp_images = get_image_files(path=path)  # get image files in the path
+    dst_path = Path(dst_path) if dst_path is not None else src_path
+    # create destination directory if not exists
+    dst_path.mkdir(parents=True, exist_ok=True)
+    # get image files in the src_path
+    fp_images = get_image_files(path=src_path)
+
 
     for color in mask_colors:
         print(f'Adding {color} mask to images ...')
@@ -38,10 +43,15 @@ def add_face_mask(src_path, mask_colors=['white', 'black', 'blue', 'red']):
                                        stderr=subprocess.PIPE)
             stdout, stderr = process.communicate()
 
-        print('Renaming images ...')
         # rename files
-        for fp in tqdm(glob.iglob(str(path / '*-with-mask*'))):
+        for fp in glob.iglob(str(src_path / '*-with-mask*')):
             src = Path(fp)
             new_name = src.name.replace('-with-mask', f'__{color}-mask__')
-            dst = path / f'{new_name}'
+            dst = dst_path / f'{new_name}'
             shutil.move(src, dst)
+
+    # copy original images to dst_path
+    if copy_originals_to_dst and src_path != dst_path:
+        for fp in fp_images:
+            dst = dst_path / fp.name
+            shutil.copy(fp, dst)
