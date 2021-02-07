b0495bdd8ccf026b3173b92685109439e532e2e5
M. Reza <ironcladgeek@hotmail.com>
2021-02-07 20:16:56 +0330

    implement multiprocessing for add_face_mask function

--- add_overlay.py
+++ add_overlay.py
@@ -3,10 +3,29 @@ from tqdm import tqdm
 from pathlib import Path
 import glob
 import shutil
-from utils import get_image_files
+from utils import get_image_files, format_time
+from multiprocessing import Pool, cpu_count
+import time
 
 
-def add_face_mask(src_path, dst_path=None, copy_originals_to_dst=True, mask_colors=['white', 'black', 'blue', 'red']):
+def put_mask(fp, color):
+    if color == 'white':
+        command = f"face-mask {fp}"
+    else:
+        command = f"face-mask {fp} --{color}"
+
+    # execute the shell command in Python
+    process = subprocess.Popen(command.split(),
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+
+def add_face_mask(src_path,
+                  dst_path=None,
+                  copy_originals_to_dst=True,
+                  mask_colors=['white', 'black', 'blue', 'red'],
+                  parallel=True):
     """
     Add face make to images if detect a face in the image. Nothing happens if no face detected in the image.
 
@@ -14,8 +33,10 @@ def add_face_mask(src_path, dst_path=None, copy_originals_to_dst=True, mask_colo
     :param dst_path (str, PosixPath): If not None, the mask added images will be saved here, otherwise, in src_path.
     :param copy_originals_to_dst (boolean): If true, copy images found in src_path to dst_path.
     :param mask_colors (list[str]): List of mask colors to be added. Could be 'white', 'black', 'blue' and 'red'.
+    :param parallel (boolean): If True, add masks to images in parallel using multiprocessing.
     """
     # check the color masks
+    s = time.time()
     for color in mask_colors:
         if color not in ['white', 'black', 'blue', 'red']:
             raise NameError('Mask colors should be white, black, blue or red.')
@@ -31,17 +52,12 @@ def add_face_mask(src_path, dst_path=None, copy_originals_to_dst=True, mask_colo
     for color in mask_colors:
         print(f'Adding {color} mask to images ...')
         # add mask to images
-        for fp in tqdm(fp_images):
-            if color == 'white':
-                command = f"face-mask {fp}"
-            else:
-                command = f"face-mask {fp} --{color}"
-
-            # execute the shell command in Python
-            process = subprocess.Popen(command.split(),
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
-            stdout, stderr = process.communicate()
+        if parallel: # multiprocessing
+            with Pool(processes=cpu_count()) as p:
+                p.starmap(put_mask, [(fp, color) for fp in fp_images])
+        else: # single process
+            for fp in tqdm(fp_images):
+                put_mask(fp, color)
 
         # rename files
         for fp in glob.iglob(str(src_path / '*-with-mask*')):
@@ -55,3 +71,6 @@ def add_face_mask(src_path, dst_path=None, copy_originals_to_dst=True, mask_colo
         for fp in fp_images:
             dst = dst_path / fp.name
             shutil.copy(fp, dst)
+
+    elapsed = format_time(time.time() - s)
+    print(f'Adding masks took: {elapsed}')
