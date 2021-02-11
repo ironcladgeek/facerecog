2b30bc95876d00887f3be4436ac8c9a64360c982
M. Reza <ironcladgeek@hotmail.com>
2021-02-11 20:08:28 +0330

    improve .dockerignore and Dockerfile
    Former-commit-id: 422b2cfd7f0a432f538d518e9976e6be8c97fed1

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM nvidia/cuda:10.1-base-ubuntu18.04
-CMD nvidia-smi
-RUN echo nvidia-smi
 
 ENV PATH="/root/miniconda3/bin:${PATH}"
 ARG PATH="/root/miniconda3/bin:${PATH}"
-RUN apt-get update
 
-RUN apt-get install -y gcc python3-dev python3-pip libxml2-dev libxslt1-dev zlib1g-dev g++
-RUN apt-get install -y wget && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install --no-install-recommends -y \
+    gcc python3-dev libxml2-dev libxslt1-dev zlib1g-dev g++ wget \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN wget \
     https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
@@ -15,8 +13,6 @@ RUN wget \
     && bash Miniconda3-latest-Linux-x86_64.sh -b \
     && rm -f Miniconda3-latest-Linux-x86_64.sh 
 
-
-RUN conda --version
 ENV PYTHONUNBUFFERED=1
 WORKDIR /app
 
@@ -24,15 +20,10 @@ WORKDIR /app
 COPY environment.yml .
 RUN conda env create -f environment.yml
 
-RUN ls
-
 # Make RUN commands use the new environment:
 SHELL ["conda", "run", "-n", "facecup", "/bin/bash", "-c"]
 
-# Make sure the environment is activated:
-RUN echo "Make sure pymongo is installed:"
-RUN python -c "import pymongo"
-
 # The code to run when container is started:
 COPY . .
+RUN ls -a
 ENTRYPOINT ["conda", "run", "--no-capture-output", "-n", "facecup", "python", "run.py"]
--- run.py
+++ run.py
@@ -19,19 +19,21 @@ GALLERY_DIR.mkdir(parents=True, exist_ok=True)
 
 # connect to MongoDB to store results in one database
 # which named your team_name and authorized with your team_pass
-client = MongoClient(host='localhost', port=27017)      # TODO: change localhost to 'mongodb'
+client = MongoClient(host='mongodb', port=27017)
 db = client.grinders
 db.authenticate('grinders', '09172226951')
-collection = db.team_name
+collection = db.grinders
 collection.remove({})
 
+print('Geeting images ...')
+print('*' * 50)
 # read the list of probe images
-probe_directory = "http://localhost/images/probe/"      # TODO: change localhost to 'nginx'
+probe_directory = "http://nginx/images/probe/"
 probe_images = requests.get(probe_directory + "images.txt").text.split()
 print("Number of probe images: " + str(len(probe_images)))
 
 # read the list of gallery images
-gallery_directory = "http://localhost/images/gallery/"  # TODO: change localhost to 'nginx'
+gallery_directory = "http://nginx/images/gallery/"
 gallery_images = requests.get(gallery_directory + "images.txt").text.split()
 print("Number of gallery images: " + str(len(gallery_images)))
 
@@ -40,7 +42,7 @@ for img_fn in probe_images:
     img_blob = requests.get(probe_directory + img_fn).content
     with open(str(PROBE_DIR / f'{img_fn}'), 'wb') as f:
         f.write(img_blob)
-print('Done\n')
+print('Done')
 
 print('Downloading gallery images ... ', end='')
 for img_fn in gallery_images:
@@ -50,9 +52,11 @@ for img_fn in gallery_images:
 print('Done\n')
 
 # get gallery/probe dataframes that contain images embeddings
-print('Processing gallery images ... \n')
+print('Processing gallery images ...')
+print('*' * 50)
 gallery_df = producer(GALLERY_DIR, do_masking=True, do_augs=True, batch_size=8)
-print('Processing probe images ... \n')
+print('Processing probe images ...')
+print('*' * 50)
 probe_df = producer(PROBE_DIR, batch_size=8)
 
 print(f'gallery_df shape: {gallery_df.shape}')
@@ -62,17 +66,21 @@ print(f'probe_df shape: {probe_df.shape}\n')
 # resulting dict is something like this:
 # {'probe1': [{"Gallery2": 0.8505}, {"Gallery3": 0.5130}, {"Gallery1": 0.0134}, ...],
 #  'probe2': [{"Gallery2": 0.8505}, {"Gallery3": 0.5130}, {"Gallery1": 0.0134}, ...],}
+print('Calculating similarities ...')
+print('*' * 50)
 similarity_dct = aligner(gallery_df, probe_df)
 
 # create a candidate list for each probe and insert it in the database
-print('Writing results to DB ...')
+print('\nWriting results to DB ...')
 for k, v in similarity_dct.items():
     candidate_list = {k: v}
     inserted_id = collection.insert_one(candidate_list).inserted_id
 
 # print to make sure that result has been saved correctly
-# for result_list in collection.find():
-    # pprint.pprint(result_list)
+print('\nThe last record in DB is:')
+print('*' * 50)
+res = [result_list for result_list in collection.find()]
+pprint.pprint(res[-1])
 
 elapsed = format_time(time.time() - s)
 print(f'\nTotal application run time: {elapsed}')
